70b50e0b66c36eb6b6c80935e337abfa1aac05f7
Darptolus <diegoroa90@gmail.com>
2023-09-18 06:24:01 -0500

    Lab 1 thread limit

--- Labs/Lab1/C/memory.c
+++ Labs/Lab1/C/memory.c
@@ -11,7 +11,7 @@ int main() {
     printf("Address of shared prior to the parallel region is: %lx\n",(unsigned long)&share);
     printf("Address of Array prior to the parallel region is: %lx\n",(unsigned long)Array);
 
-    #pragma omp parallel private(i, Array) shared(share)
+    #pragma omp parallel private(i, Array) shared(share) num_threads(4)
     {
         printf("Address of i as seen by thread %d: %lx\n", omp_get_thread_num(), (unsigned long)&i);
         printf("Address of Array as seen by thread %d: %lx\n", omp_get_thread_num(), (unsigned long)Array);
